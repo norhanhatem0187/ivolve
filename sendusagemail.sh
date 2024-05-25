6fd482c0fbb9e4827a78aca09220deed8c130609
norhanhatem0187 <norhan.hatem0187@gmail.com>
2024-05-25 14:34:48 +0300

    day11

--- sendusagemail.sh
+++ sendusagemail.sh
@@ -5,7 +5,7 @@ TO_ADDRESS="norhan.hatem0187@gmail.com"
 SUBJECT="Disk Space Alert"
 
 # Threshold percentage for disk usage
-THRESHOLD=5
+THRESHOLD=05
 
 # Get current disk usage
 DISK_USAGE=$(df -h | grep -E '^/dev/' | awk '{print $5 " " $1}' | sort -n | tail -1 | awk '{print $1}' | sed 's/%//')
